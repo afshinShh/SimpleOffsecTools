cd5e8b37a049cbac0daa4afea1c1274c9cb45c0e
afshinSh <afshintheprojects@gmail.com>
2023-12-18 09:16:40 +0330

    main func

--- ssh_bruteforcer.py
+++ ssh_bruteforcer.py
@@ -2,6 +2,7 @@ import argparse
 from termcolor import colored,cprint 
 import asyncio 
 import os
+from datetime import datetime 
 
 def get_args():
     """Function to get command-line arguments""" 
@@ -21,8 +22,39 @@ def get_args():
     return argument
 
 
+async def main(hostname, port, username, wordlist):
+    
+    tasks = []
+    passwords = []
+    found_flag = asyncio.Event()
+    concurrency_limit = 10
+    counter = 0
+    
+    with open(wordlist, 'r') as f:
+        
+        for password in f.readlines():
+            password = password.strip()
+            passwords.append(password)
+        for password in passwords:
+            if counter >= concurrency_limit:
+                await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+                
+                tasks =  []
+                counter = 0
+                
+                if not found_flag.set():
+                    tasks.append(asyncio.create_task(
+                        ssh_bruteforce(hostname, username, password, port, found_flag)))    
+                    
+            await asyncio.sleep(0.01)
+            
+            counter += 1
+                    
+def ssh_bruteforce(hostname, username, password, port, found_flag):
+    pass
+
 if __name__ == "__main__":
-    print(colored("Afshin Here!!!","green"))
+    print(colored("\nAfshin Here!!!","green"))
     cprint("Welcome to my SSH Bruteforcer","blue")
     
     arguments = get_args()
@@ -32,9 +64,9 @@ if __name__ == "__main__":
             "[-] Wordlist location is not right","red"))
         exit(1)
         
-    print("\n--------------------------------------------------------\n--------------------------------------------------------")
-    print(colored(f"[*] Target\t: ", "magenta"), arguments.target)
-    print(colored(f"[*] Username\t: ", "magenta"), arguments.username)
+    print("--------------------------------------------------------\n--------------------------------------------------------")
+    print(colored(f"[*] Target\t:", "magenta"), arguments.target)
+    print(colored(f"[*] Username\t:", "magenta"), arguments.username)
     print(colored(
         f"[*] Port\t: ", "magenta"), end="")
     print('22' if not arguments.port else arguments.port)
@@ -43,11 +75,11 @@ if __name__ == "__main__":
     print(arguments.wordlist)
     print(colored(f"[*] Protocol\t: ", "magenta"), end="")
     print("SSH")
-    print("--------------------------------------------------------\n--------------------------------------------------------")
+    print("--------------------------------------------------------")
     
     print(colored(
         f"SSH-Bruteforce starting at {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}", 'yellow'))
-    print("\n--------------------------------------------------------\n--------------------------------------------------------")
+    print("--------------------------------------------------------\n--------------------------------------------------------")
     #asyncio.run(main(arguments.target, arguments.port,
     #            arguments.username, arguments.wordlist))
     
