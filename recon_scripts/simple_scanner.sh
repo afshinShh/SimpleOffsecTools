7a613c7c85b30181d7af3167ec16e2f06c593858
afshinSh <afshintheprojects@gmail.com>
2023-12-19 15:51:00 +0330

    simple_scanner.sh v2

--- recon_scripts/simple_scanner.sh
+++ recon_scripts/simple_scanner.sh
@@ -1,8 +1,12 @@
 # source -> bug bounty bootcamp book from Vickie Li -> chapter5/ Writing your own recon scrips 
 
 #!/bin/bash
+echo "Creating directory $1_recon"
+mkdir $1_recon
 
-nmap $1
+nmap $1 > $1_recon/nmap.txt
+echo "results of nmap scanner saved in $1_recon/nmap.txt"
 
 # export PATH="PATH_TO_DIRSEARCH:$PATH"
-python3 ~/tools/bugbounty/dirsearch.py -u $1 -e php
+python3 ~/tools/bugbounty/dirsearch.py -u $1 -e php  --simple-report $1_recon/dirsearch.txt
+echo "results of dirsearch saved in $1_recon/dirsearch.txt"
