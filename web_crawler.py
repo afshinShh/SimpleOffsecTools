313a3a7cd062d50f47a841ae6ffd2feffa72d1c1
afshinSh <afshintheprojects@gmail.com>
2024-01-09 06:51:01 +0330

    main

--- web_crawler.py
+++ web_crawler.py
@@ -12,3 +12,20 @@ def get_args():
     parser.add_argument('-d','--depth',dest='depth',type=int, default=1,
                         help="Depth of crawling, default is 1")
     return parser.parse_args()
+
+class WebCrawler:
+    def __init__(self, url, depth):
+        pass
+    def print_banner(self):
+        pass
+    def start_crawling(self):
+        pass
+    def print_results(self):
+        pass
+
+if __name__ == '__main__':
+    args = get_args()
+    web_crawler = WebCrawler(args.url, args.depth)
+    web_crawler.print_banner()
+    web_crawler.start_crawling()
+    web_crawler.print_results()
