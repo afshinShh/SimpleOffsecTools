2c07df44dc99e557c4bf88a82069bf5b2359c771
afshinSh <afshintheprojects@gmail.com>
2024-01-12 05:38:58 +0330

    main functionality

---
+++ pdf_bruteforcer.py
@@ -0,0 +1,27 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from os import path
+from sys import exit
+from termcolor import colored
+
+def bruteforcer(pdfFile, wordlistFile):
+    pass
+
+def decrypt_pdf(encrypted_file, decrypted_file_name, password):
+    pass
+
+if __name__ == "__main__":  
+    pdfFile = input("Enter the location of the password protected pdf file: ") 
+    if not path.exists(pdfFile):
+        print(colored("[-] Please provide a valid pdf file location!",'red'))
+        exit(1)
+
+    wordlistFile = input("Enter the location of the wordlist file: ")
+    if not path.exists(wordlistFile):
+        print(colored("[-] Please provide a valid wordlist file location!",'red'))
+        exit(1)
+    
+    password = bruteforcer(pdfFile, wordlistFile)
+    if password:
+        decrypt_pdf(pdfFile, f"decrypted-{path.basename(pdfFile)}", password)
+    else:
+        print(colored("[-] Password was not Found", 'red'))
